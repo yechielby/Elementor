6170ce3803582cdc1572e9b22a6a4d6c55e3559a
yechielby <yechiel.by@gmail.com>
2020-06-22 01:26:29 +0300

    fix single content width

--- twentytwenty-child/template-parts/content-products.php
+++ twentytwenty-child/template-parts/content-products.php
@@ -28,86 +28,97 @@ $sele_price = get_product_sele_price($post_id);
 	<?php
 	get_template_part( 'template-parts/featured-image' );
 	echo '<p class="price">$' . ( $is_on_sale ? $price.'<span>$'.$sele_price.'</span>' : $price ) . '</p>';
-	
-	echo '<div class="section-inner section-description">';
-	echo 	'<h2 class="widget-title subheading heading-size-3">'.__('Product Description', 'twentytwentychild' ).'</h2>';
-	echo	'<p class="heading-size-5">'.$description.'</p>';
-	if ($video) {
-	echo 	'<iframe  class="product-video"  width="1280" height="720" src="'.$video.'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-	}
-	echo '</div>';
 	?>
 
-	<div class="section-inner">
-		<?php
-		wp_link_pages(
-			array(
-				'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__( 'Page', 'twentytwenty' ) . '"><span class="label">' . __( 'Pages:', 'twentytwenty' ) . '</span>',
-				'after'       => '</nav>',
-				'link_before' => '<span class="page-number">',
-				'link_after'  => '</span>',
-			)
-		);
+	<div class="post-inner thin">
+		<div class="entry-content">
+			<?php
+			echo '<div class="section-inner section-description">';
+			echo 	'<h2 class="widget-title subheading heading-size-3">'.__('Product Description', 'twentytwentychild' ).'</h2>';
+			echo	'<p class="heading-size-5">'.$description.'</p>';
+			if ($video) {
+			echo 	'<iframe  class="product-video"  width="1280" height="720" src="'.$video.'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+			}
+			echo '</div>';
+			?>
 
-		edit_post_link();
+			
+			<?php the_content( __( 'Continue reading', 'twentytwenty' ) ); ?>
+				
 
-		// Single bottom post meta.
-		twentytwenty_the_post_meta( get_the_ID(), 'single-bottom' );
+			<div class="section-inner">
+				<?php
+				wp_link_pages(
+					array(
+						'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__( 'Page', 'twentytwenty' ) . '"><span class="label">' . __( 'Pages:', 'twentytwenty' ) . '</span>',
+						'after'       => '</nav>',
+						'link_before' => '<span class="page-number">',
+						'link_after'  => '</span>',
+					)
+				);
 
-		if ( post_type_supports( get_post_type( get_the_ID() ), 'author' ) && is_single() ) {
+				edit_post_link();
 
-			get_template_part( 'template-parts/entry-author-bio' );
+				// Single bottom post meta.
+				twentytwenty_the_post_meta( get_the_ID(), 'single-bottom' );
 
-		}
-		?>
+				if ( post_type_supports( get_post_type( get_the_ID() ), 'author' ) && is_single() ) {
 
-	</div><!-- .section-inner -->
+					get_template_part( 'template-parts/entry-author-bio' );
 
-	<?php
-	// Related Products
-	$categories = wp_get_post_terms($post_id, 'custom_categories');
-	// get the list category in this product.
-	$categories_ids = array();
-	foreach ($categories as $category) {
-		$categories_ids[] = $category->term_id;
-	}
-	// The Query
-	$the_query = new WP_Query( array(
-		'post_type' => 'products',
-		'post_status' => 'publish',
-		'post__not_in'=> array($post_id),
-		'tax_query' => array(array(
-			'taxonomy' => 'custom_categories',
-			'field' => 'term_id',
-			'terms' => $categories_ids,
-			'operator' => 'IN'
-		)),
-		'orderby' => 'date',
-		'order' => 'DESC',
-		'posts_per_page' => 3
-	));
+				}
+				?>
 
-	// The Loop
-	if ( $the_query->have_posts() ) {
-		echo '<div class="section-inner section-categories">'.
-				'<h2 class="widget-title subheading heading-size-3">'.__('Related Products', 'twentytwentychild' ).'</h2>'.
-				'<div class="grid">';
-					while ( $the_query->have_posts() ) {
-						$the_query->the_post();
-						echo '<a href="' . esc_url( get_permalink() ) . '" class="grid-column">';
-							get_template_part( 'template-parts/featured-image' );
-							the_title( '<p class="entry-title grid-title">', '</p>' );
-							if ( get_product_is_on_sale( get_the_ID() ) ) {
-							echo '<div class="sale"><span>'.__('SALE', 'twentytwentychild' ).'</span></div>';
+			</div><!-- .section-inner -->
+
+			<?php
+			// Related Products
+			$categories = wp_get_post_terms($post_id, 'custom_categories');
+			// get the list category in this product.
+			$categories_ids = array();
+			foreach ($categories as $category) {
+				$categories_ids[] = $category->term_id;
+			}
+			// The Query
+			$the_query = new WP_Query( array(
+				'post_type' => 'products',
+				'post_status' => 'publish',
+				'post__not_in'=> array($post_id),
+				'tax_query' => array(array(
+					'taxonomy' => 'custom_categories',
+					'field' => 'term_id',
+					'terms' => $categories_ids,
+					'operator' => 'IN'
+				)),
+				'orderby' => 'date',
+				'order' => 'DESC',
+				'posts_per_page' => 3
+			));
+
+			// The Loop
+			if ( $the_query->have_posts() ) {
+				echo '<div class="section-inner section-categories">'.
+						'<h2 class="widget-title subheading heading-size-3">'.__('Related Products', 'twentytwentychild' ).'</h2>'.
+						'<div class="grid">';
+							while ( $the_query->have_posts() ) {
+								$the_query->the_post();
+								echo '<a href="' . esc_url( get_permalink() ) . '" class="grid-column">';
+									get_template_part( 'template-parts/featured-image' );
+									the_title( '<p class="entry-title grid-title">', '</p>' );
+									if ( get_product_is_on_sale( get_the_ID() ) ) {
+									echo '<div class="sale"><span>'.__('SALE', 'twentytwentychild' ).'</span></div>';
+									}
+								echo '</a>';
 							}
-						echo '</a>';
-					}
-				echo '</div>'.
-			'</div>';
-	}
-	wp_reset_postdata();
+						echo '</div>'.
+					'</div>';
+			}
+			wp_reset_postdata();
 
-	?>
+			?>
+
+		</div><!-- .entry-content -->
+	</div><!-- .post-inner -->
 
 	<?php
 
